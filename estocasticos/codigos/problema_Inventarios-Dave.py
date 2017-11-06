1aa4bb7a9664cbcb520edc2355c66bbfff6a1b4b
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-11-06 13:06:57 -0500

    Estocasticos: Agrega codigo para problema de inventarios de Dave.

---
+++ estocasticos/codigos/problema_Inventarios-Dave.py
@@ -0,0 +1,22 @@
+"""
+@author: Luis I. Reyes Castro
+
+Problema de Manejo de Inventario de Dave
+
+"""
+
+import numpy as np
+from ModelosEstocasticos import CadenaDeMarkov
+
+# Lista de estados
+S = [ 0, 1, 2, 3 ]
+# Matriz de transicion
+P = np.array( [ [ 0.08, 0.18, 0.37, 0.37 ],
+                [ 0.63, 0.37, 0.,   0.   ],
+                [ 0.26, 0.37, 0.37, 0.   ],
+                [ 0.08, 0.18, 0.37, 0.37 ] ] )
+
+# Crea una nuevo objeto que representa una Cadena de Markov
+cadena = CadenaDeMarkov( S, P)
+# Computa la distribucion estacionaria (i.e. en estado estable) de la cadena
+pi_estrella = cadena.distribucion_estacionaria()
