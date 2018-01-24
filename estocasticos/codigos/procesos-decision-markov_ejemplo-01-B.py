58970d772a16f4f53ccee10cf06bcecb8d892697
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2018-01-24 11:02:24 -0500

    Estocasticos: Empieza ejemplo-01-B de PDMs.

---
+++ estocasticos/codigos/procesos-decision-markov_ejemplo-01-B.py
@@ -0,0 +1,86 @@
+"""
+Implementacion del Proceso de Decision Markoviano (PDM) del
+Problema prototipo de la Seccion 19.1 del texto de Hillier & Lieberman (9ED)
+
+@author: Luis I. Reyes Castro
+
+"""
+
+import numpy as np
+from ModelosEstocasticos import CadenaDeMarkov
+
+# Declaramos los estados
+S = [ 0, 1, 2, 3]
+# Declaramos las acciones disponibles en cada estado
+A = { 0 : [ 'Nada' ],
+      1 : [ 'Nada', 'Remplazo' ],
+      2 : [ 'Nada', 'Reparacion-general', 'Remplazo' ],
+      3 : [ 'Remplazo' ] }
+# Declaramos las transiciones
+T = { ( 0, 'Nada') : { 1 : 7/8, 2 : 1/16, 3 : 1/16 },
+      ( 1, 'Nada') : { 1 : 3/4, 2 :  1/8, 3 : 1/8  },
+      ( 1, 'Remplazo') : { 0 : 1 },
+      }
+
+
+# Declaramos una funcion que toma como argumento una politica factible y
+# retorna la Cadena de Markov inducida por la politica junto con la funcion
+# de costo promedio por estado
+def CadenaDeMarkovInducida( accion_e1 = 'Nada',
+                            accion_e2 = 'Nada' ) :
+
+    # Declaramos los estados y matriz de transicion
+
+    P = np.zeros( shape = (4,4) )
+    # Declaramos el diccionario de costo promedio por estado
+    C = { estado : 0.0 for estado in S }
+
+    # Estado 0
+    P[0,:] = [ 0.0, 7.0/8, 1.0/16, 1.0/16]
+    C[0]   = 0.0
+
+    # Estado 1
+    if accion_e1 == 'Nada' :
+        P[1,:] = [ 0.0, 3.0/4, 1.0/8, 1.0/8]
+        C[1]   = 1000.0
+    elif accion_e1 == 'Remplazo':
+        P[1,:] = [ 1.0, 0.0, 0.0, 0.0]
+        C[1]   = 6000.0
+
+    # Estado 2
+    if accion_e2 == 'Nada' :
+        P[2,:] = [ 0.0, 0.0, 1.0/2, 1.0/2]
+        C[2]   = 3000.0
+    elif accion_e2 == 'Reparacion-general' :
+        P[2,:] = [ 0.0, 1.0, 0.0, 0.0]
+        C[2]   = 4000.0
+    elif accion_e2 == 'Remplazo' :
+        P[2,:] = [ 1.0, 0.0, 0.0, 0.0]
+        C[2]   = 6000.0
+
+    # Estado 3
+    P[3,:] = [ 1.0, 0.0, 0.0, 0.0]
+    C[3]   = 6000.0
+
+    return ( S, P, C)
+
+# Escogemos una politica
+accion_e1 = 'Nada'
+accion_e2 = 'Reparacion-general'
+# Construimos la Cadena de Markov inducida por la politica
+( S, P, C) = CadenaDeMarkovInducida( accion_e1, accion_e2)
+cadena     = CadenaDeMarkov( S, P)
+
+# Calculamos el costo promedio que incurre la politica escogida
+pi_estrella    = cadena.distribucion_estacionaria()
+costo_politica = 0.0
+for estado in S :
+    costo_politica += pi_estrella[estado] * C[estado]
+
+# Imprimimos la politica y su costo promedio
+print( 'POLITICA' )
+print( 'Estado: 0 -> Accion: Nada' )
+print( 'Estado: 1 -> Accion: ' + accion_e1 )
+print( 'Estado: 2 -> Accion: ' + accion_e2 )
+print( 'Estado: 3 -> Accion: Remplazo' )
+print( 'Costo promedio por periodo: ' + str(costo_politica) )
